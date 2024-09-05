42f3d4a5e2b08ba11a7dc7667d2612f323b736cf
Vinicius Leopoldino <135030968+ViniciusLeopoldino@users.noreply.github.com>
2024-09-05 22:32:04 +0000

    chore: Add form submission handling to FaleConosco page

--- portoAutoCheck-v3/portoAutoCheck/src/pages/FaleConosco.tsx
+++ portoAutoCheck-v3/portoAutoCheck/src/pages/FaleConosco.tsx
@@ -3,11 +3,16 @@ import Form from '../components/Form/Form';
 import Input from '../components/Input/Input';
 import Button from '../components/Button/Button';
 
+const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  event.preventDefault();
+  alert('Mensagem enviada!');
+}
+
 const FaleConosco: React.FC = () => {
   return (
     <div>
       <h1>Fale Conosco</h1>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Input type="text" id="nome" name="nome" placeholder="Nome" />
         <Input type="email" id="email" name="email" placeholder="E-mail" />
         <textarea id="mensagem" name="mensagem" placeholder="Deixe sua mensagem" />
