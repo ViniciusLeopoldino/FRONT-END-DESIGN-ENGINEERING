deee8c6cdd1d0c5fd5722891261b77db588a4e6a
Vinicius Leopoldino <135030968+ViniciusLeopoldino@users.noreply.github.com>
2024-09-11 01:20:04 +0000

    chore: Update Integrantes page with new member images
    Update the Integrantes page to use new member images for Diego, Pablo, and Vinicius. The images are now imported from the assets folder instead of being referenced with relative paths. This improves the maintainability of the code and ensures that the images are properly displayed.

--- portoAutoCheck-v3/portoAutoCheck/src/pages/Integrantes.tsx
+++ portoAutoCheck-v3/portoAutoCheck/src/pages/Integrantes.tsx
@@ -3,26 +3,30 @@ import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import Integrantes from '../components/Integrantes/Integrantes';
 import styles from '../styles/pages/Integrantes.module.css';
+import image from '../assets/Diego.jpeg';
+import image2 from '../assets/Pablo.jpeg';
+import image3 from '../assets/Vinicius.jpg';
+
 
 const integrantesData = [
   {
     name: 'Diego Santos',
     rm: '558711',
-    image: '../img/Diego.jpeg',
+    image: image,
     github: 'https://github.com/sashabiceps',
     linkedin: 'https://www.linkedin.com/in/diego-santos-cardoso-51329430b/',
   },
   {
     name: 'Pablo Lopes',
     rm: '556834',
-    image: '../img/Pablo.jpeg',
+    image: image2,
     github: 'https://github.com/Pablo0703',
     linkedin: 'https://www.linkedin.com/in/pablo-lopes-09a66a275/',
   },
   {
     name: 'Vinicius Leopoldino',
     rm: '557047',
-    image: '../img/Vinicius.jpg',
+    image: image3,
     github: 'https://github.com/ViniciusLeopoldino',
     linkedin: 'https://www.linkedin.com/in/vinicius-leopoldino-de-oliveira-9a2194120/',
   },
